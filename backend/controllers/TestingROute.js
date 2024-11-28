dfd3eca8508353e08bbb95f41483f2e25589b52e
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-11-28 12:40:04 +0530

    tesing route / modified

--- backend/controllers/TestingROute.js
+++ backend/controllers/TestingROute.js
@@ -1,10 +1,10 @@
-
 const TestingROute = async (req, res) => {
-  try {
-    res.status(200).json({ testing: "/ route testing" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export default TestingROute;
+    try {
+      res.status(200).json({ testing: "/ route testing" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  export default TestingROute;
+  
