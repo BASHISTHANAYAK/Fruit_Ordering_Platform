f3940a1d30841219bbbadc947ee1131458af25f7
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-12-01 22:07:12 +0530

    middlewares updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,8 @@ import "./db_connection/mongoo.js";
 const app = express();
 app.use(cors({ origin: "*" }));
 dotenv.config();
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.use(router);
 
 const PORT = process.env.PORT || 7000;
